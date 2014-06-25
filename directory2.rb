d7c79e4467363d85f19999cc71910850a40ad97d
TalalK <Talal@Talals-MacBook-Pro.local>
2014-06-25 18:25:20 +0100

    first commit

--- directory2.rb
+++ directory2.rb
@@ -4,33 +4,53 @@ def print_header
   p "-------------"
 end
 
+@line_width = 50
+
 def print(students)
-  students.until do |student|
-    p "#{student[:name]} #{student[:cohort]} cohort, #{student[:hobby]}, #{student[:nationality]}" if student[:name].start_with?("C", "c") && student[:name].length < 12
-  end
+	count = 0
+  	while count < students.length do
+    	p "#{students[count][:name]} #{students[count][:cohort]} cohort, #{students[count][:hobby]}".center(@length_width) if students[count][:name].start_with?("C", "c") && students[count][:name].length < 12
+   	 	count = count + 1
+  	end
 end
+# changed student.each() to print using a while loop
 
 def print_footer(names)    
   p "Overall, we have #{names.length} great students"
 end
 
 def input_students
-  p "Please enter the names of the students"
-  p "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do    
-    # add the student hash to the array
-    students << {:name => name, :cohort => :July} if #{student:name}
-    p "Now we have #{students.length} students"
-    # get another name from the user
-    name = gets.chomp
-  end
-  # return the array of students
-  students
+	p "Please enter the name of the student"
+	# create an empty array
+ 	students = []
+ 	# get the first name
+ 	name = gets.gsub(/\n/,"").capitalize
+ 	while !name.empty? do
+ 		# while the name is not empty, repeat this code
+ 		loop do
+ 			require "date"
+		p "Please enter the Month of the Cohort"
+		# get cohort
+		@cohort = gets.chomp.capitalize.to_sym
+		break if Date::MONTHNAMES.include?(@cohort.to_s)
+		end
+		# Looping until they get the date corret.		
+		p "Please enter the Hobby of the student"	
+		# get hobby
+		hobby = gets.chomp.capitalize.to_sym
+		# add new values to array
+		students << {:name => name, :cohort =>@cohort, :hobby => hobby}
+		if students.length == 1
+
+			p "Now we have #{students.length} student"	
+			else p "Now we have #{students.length} students"	
+		end
+		p "Please enter the name of another student, or press return to finish"
+		# get another name
+		name = gets.chomp.capitalize
+	end
+	students
+
 end
 
 students = input_students
